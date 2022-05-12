4642767173269018e6912acd950c294b8e44fb0e
andricasaputra <dricakentrink@gmail.com>
2022-05-12 10:56:54 +0800

    add headers cors

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -8,9 +8,12 @@ class Cors
 {
     public function handle($request, Closure $next)
     {
-        return $next($request)
-            ->headers->set("Access-Control-Allow-Origin", "*")
-            ->headers->set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
-            ->headers->set("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, X-Token-Auth, Authorization");
+        $response = $next($request);
+
+        $response->headers->set('Access-Control-Allow-Origin' , '*');
+        $response->headers->set('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT, DELETE');
+        $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Accept, Authorization, X-Requested-With, Application');
+        
+        return $response;
     }
 }
